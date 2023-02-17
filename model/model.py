134d6d60de206b7c4a917304cade4d158e2df039
isuri97 <you@example.com>
2023-02-17 09:43:32 +0530

    change the source code

--- model/model.py
+++ model/model.py
@@ -1,15 +1,20 @@
-import numpy as np
 import pandas as pd
-from scipy.special import softmax
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import argparse
 
 from simpletransformers.ner import NERModel, NERArgs
 
+from contextlib import redirect_stdout
+
+from utils.label_converter import decode, encode
+
+from utils.print_information import print_information_multi_class, print_information
+
 parser = argparse.ArgumentParser(
     description='''evaluates multiple models  ''')
 parser.add_argument('--model_name', required=False, help='model name', default="bert")
-parser.add_argument('--model_type', required=False, help='model type', default="bert-base-uncased")
+parser.add_argument('--model_type', required=False, help='model type', default="bert-base-cased")
 parser.add_argument('--cuda_device', required=False, help='cuda device', default=0)
 parser.add_argument('--train', required=False, help='train file', default='data/sample.txt')
 # parser.add_argument('--test', required=False, help='test file', default='data/SOLD_test.tsv')
@@ -62,23 +67,84 @@ print(df1.isnull().values.any())
 df1 = df1.dropna()
 print(df1.isnull().values.any())
 
+print(len(df1))
+print('---')
+
+df1 = df1[~df1['words'].str.contains('\[')]
+df1 = df1[~df1['words'].str.contains('\]')]
+df1 = df1[~df1['words'].str.contains('\'')]
+df1 = df1[~df1['words'].str.contains('\(')]
+df1 = df1[~df1['words'].str.contains('\)')]
+df1 = df1[~df1['words'].str.contains('-')]
+df1 = df1[~df1['words'].str.contains(';')]
+df1 = df1[~df1['words'].str.contains(' ')]
+df1 = df1[~df1['words'].str.contains('“')]
+df1 = df1[~df1['words'].str.contains('”')]
+df1 = df1[~df1['words'].str.contains(',')]
+df1 = df1[~df1['words'].str.contains('’')]
+df1 = df1[~df1['words'].str.contains(':')]
+df1 = df1[~df1['words'].str.contains(' ')]
+
+df1.to_csv('testing.csv')
+# df_train, df_test = [x for _, x in df1.groupby(df1['sentence_id'] <= 190)]
+# print(len(df_train))
+
+df_train= df1
+df_test = df1
+# df_test = df_test.loc[(df_test['sentence_id'] >= 187) & (df_test['sentence_id'] < 188)]
+# print(len(df_test))
+# df_test = pd.read_csv('data/373.txt')
+# df_test = df_test[~df_test['words'].str.contains('\'')]
+# df_test=df_test[~df_test['words'].str.contains('\[')]
+# df_test=df_test[~df_test['words'].str.contains('\]')]
+
+print(len(df_train))
+print(len(df_test))
+
+# concatenate words till . and add comma
+words = df_test['words']
+sentence_ids = df_test['sentence_id']
+
+df_test
+
+df_test = df_test.astype({'labels': 'string'})
+print(df_test.dtypes)
+
+sentences = []
+ids = []
+sentence = ""
+for word, s_id in zip(words.to_list(), sentence_ids.to_list()):
+    sentence += word + " "
+    if word == "." or word == "?" or word == "!":
+        sentences.append(sentence.strip())
+        ids.append(s_id)
+        sentence = ""
+
+# sentences = " ".join(words).split(".")
+# sentences = [sentence.strip() + "." for sentence in sentences if sentence.strip()]
+
+# sentences=sentences[0:5]sentence
+sentences
 
 # using the train test split function
-X_train, y_train,= train_test_split(df1,random_state=777,test_size=0.1)
+# X_train, y_train,= train_test_split(df1,test_size=0.1)
+# df_train['labels'] = encode(df_train["labels"])
+# df_test['labels'] = encode(df_test["labels"])
 
+train_df = pd.DataFrame(df_train, columns=["sentence_id", "words", "labels"])
+eval_df = pd.DataFrame(df_test, columns=["sentence_id", "words", "labels"])
 
-train_df = pd.DataFrame(X_train, columns=["sentence_id", "words", "labels"])
-eval_df = pd.DataFrame(y_train, columns=["sentence_id", "words", "labels"])
-
-
-model_args = NERArgs()
-model_args.labels_list = ['O', 'B-DATE', 'B-PERSON', 'B-GPE', 'B-ORG', 'I-ORG', 'B-CARDINAL', 'B-LANGUAGE',
-                          'B-EVENT', 'I-DATE', 'B-NORP', 'B-TIME', 'I-TIME', 'I-GPE', 'B-ORDINAL', 'I-PERSON', 'B-MILITARY',
-                          'I-MILITARY', 'I-NORP', 'B-CAMP', 'I-EVENT', 'I-CARDINAL', 'B-LAW', 'I-LAW', 'B-QUANTITY', 'B-RIVER',
-                          'I-RIVER', 'B-PERCENT', 'I-PERCENT', 'B-WORK_OF_ART', 'I-QUANTITY', 'B-FAC', 'I-FAC', 'I-WORK_OF_ART',
-                          'B-MONEY', 'I-MONEY', 'B-STREET', 'I-STREET', 'B-LOC', 'B-GHETTO', 'B-SEA-OCEAN', 'I-SEA-OCEAN',
-                          'B-PRODUCT', 'I-CAMP', 'I-LOC', 'I-PRODUCT', 'I-GHETTO', 'B-SPOUSAL', 'I-SPOUSAL', 'B-SHIP', 'I-SHIP',
-                          'B-FOREST', 'I-FOREST', 'B-GROUP', 'I-GROUP', 'B-MOUNTAIN', 'I-MOUNTAIN']
+model_args = {
+    'train_batch_size': 32,
+    'eval_batch_size': 8,
+}
+# model_args.labels_list = ['O', 'B-DATE', 'B-PERSON', 'B-GPE', 'B-ORG', 'I-ORG', 'B-CARDINAL', 'B-LANGUAGE',
+#                           'B-EVENT', 'I-DATE', 'B-NORP', 'B-TIME', 'I-TIME', 'I-GPE', 'B-ORDINAL', 'I-PERSON', 'B-MILITARY',
+#                           'I-MILITARY', 'I-NORP', 'B-CAMP', 'I-EVENT', 'I-CARDINAL', 'B-LAW', 'I-LAW', 'B-QUANTITY', 'B-RIVER',
+#                           'I-RIVER', 'B-PERCENT', 'I-PERCENT', 'B-WORK_OF_ART', 'I-QUANTITY', 'B-FAC', 'I-FAC', 'I-WORK_OF_ART',
+#                           'B-MONEY', 'I-MONEY', 'B-STREET', 'I-STREET', 'B-LOC', 'B-GHETTO', 'B-SEA-OCEAN', 'I-SEA-OCEAN',
+#                           'B-PRODUCT', 'I-CAMP', 'I-LOC', 'I-PRODUCT', 'I-GHETTO', 'B-SPOUSAL', 'I-SPOUSAL', 'B-SHIP', 'I-SHIP',
+#                           'B-FOREST', 'I-FOREST', 'B-GROUP', 'I-GROUP', 'B-MOUNTAIN', 'I-MOUNTAIN']
 
 model_args.overwrite_output_dir = True
 model_args.reprocess_input_data = True
@@ -97,20 +163,124 @@ model_args.reprocess_input_data = True
 MODEL_NAME = arguments.model_name
 MODEL_TYPE = arguments.model_type
 cuda_device = int(arguments.cuda_device)
-
+# MODEL_TYPE, MODEL_NAME,
 model = NERModel(
-    MODEL_TYPE, MODEL_NAME,
+    MODEL_TYPE, 'outputs/',
     args=model_args,
+    labels=['O', 'B-DATE', 'B-PERSON', 'B-GPE', 'B-ORG', 'I-ORG', 'B-CARDINAL', 'B-LANGUAGE',
+            'B-EVENT', 'I-DATE', 'B-NORP', 'B-TIME', 'I-TIME', 'I-GPE', 'B-ORDINAL', 'I-PERSON', 'B-MILITARY',
+            'I-MILITARY', 'I-NORP', 'B-CAMP', 'I-EVENT', 'I-CARDINAL', 'B-LAW', 'I-LAW', 'B-QUANTITY', 'B-RIVER',
+            'I-RIVER', 'B-PERCENT', 'I-PERCENT', 'B-WORK_OF_ART', 'I-QUANTITY', 'B-FAC', 'I-FAC', 'I-WORK_OF_ART',
+            'B-MONEY', 'I-MONEY', 'B-STREET', 'I-STREET', 'B-LOC', 'B-GHETTO', 'B-SEA-OCEAN', 'I-SEA-OCEAN',
+            'B-PRODUCT', 'I-CAMP', 'I-LOC', 'I-PRODUCT', 'I-GHETTO', 'B-SPOUSAL', 'I-SPOUSAL', 'B-SHIP', 'I-SHIP',
+            'B-FOREST', 'I-FOREST', 'B-GROUP', 'I-GROUP', 'B-MOUNTAIN', 'I-MOUNTAIN']
 )
 # Train the model
-model.train_model(train_df)
+# model.train_model(train_df)
+# model.save_model(output_dir='outputs/saved_model/')
 
 # Evaluate the model
-result, model_outputs, predictions = model.eval_model(eval_df)
-print('#####')
-print(result)
+# result, model_outputs, predictions = model.eval_model(eval_df)
+# print('#####')
+# print(result)
+
+predictions, outputs = model.predict(sentences)
+
+predictions = []
+with open('filex.txt', 'a') as f:
+    for h, id in zip(sentences, ids):
+        sentence_lst = []
+        sentence_lst.append(h)
+        preds, outputs = model.predict(sentence_lst)
+        predictions.append(preds)
+        # if len(df_test.groupby('sentence_id').get_group(id)) != len(predictions):
+        if len(h.split(' ')) != len(preds[0]):
+            f.write(h)
+            print(h)
+print(len(preds))
+
+print(len(eval_df['labels']))
+pred_list = []
+# for pred in predictions:
+#     for tag in pred:
+#         pred_list.append(tag.values())
+#
+#
+# lab=eval_df['labels'].tolist()
+# for i in range(0,len(pred_list)):
+#     if pred_list[i]!=lab[i]:
+#         print(f'not matched index {i}, preadval = {predictions[i]}, gold val = {lab[i]}')
 
 
+ll = []
+key_list = []
+for i in predictions:
+    for lst in i[0]:
+        # for (k,v) in lst:
+        ll += list(lst.values())
+        key_list += list(lst.keys())
+        # key_list.append(lst.keys())
+
+new1 = pd.DataFrame()
+new1['w'] = key_list
+new1['w'].to_csv('pred_key-list.csv')
+
+# for i in predictions:
+#     for h in i:
+#         for v in h.keys():
+#             key_list.append(v)
+
+# for i in range(0,len(key_list)):
+#     if key_list[i]!= words[i]:
+#         print(f'not matched index {i}, preadval = {key_list[i]}, gold val = {words[i]}')
+
+
+print('------')
+print(len(ll))
+print('======')
+# print(key_list)
+
+w_list = words.to_list()
+new2 = pd.DataFrame()
+new2['w'] = w_list
+new2['w'].to_csv('word-list.csv')
+w_list.to_csv('word-list.csv')
+
+
+# for word in words:
+#     w_list.append(word)
+
+
+# missing_words = set(w_list) - set(key_list)
+# print(missing_words)
+# #
+# missing_indexes_and_words = []
+# for i, word in enumerate(words):
+#     if word not in key_list:
+#         missing_indexes_and_words.append((i, word))
+#
+# print(len(missing_indexes_and_words))
+# new = pd.DataFrame()
+# new['sub'] = missing_indexes_and_words
+#
+# new['sub'].to_csv('te.csv')
+#
+# eval_df['labels'] = decode(eval_df['labels'])
+# eval_df['predictions'] = decode(eval_df['predictions'])
+
+# print_information(eval_df,'predictions','labels')
+
+eval_df['predictions'] = ll
+
+y_true = eval_df['labels']
+y_pred = eval_df['predictions']
+
+# print(metrics.confusion_matrix(y_true, y_pred))
+print(metrics.classification_report(y_true, y_pred, digits=7))
+
+# print_information_multi_class(eval_df,'predictions','labels')
+# sentences = ["Some arbitary sentence", "Simple Transformers sentence"]
+# predictions, raw_outputs = model.predict(sentences)
 # #
 # # Predictions on arbitary text strings
 # sentences = ["Some arbitary sentence", "Simple Transformers sentence"]
