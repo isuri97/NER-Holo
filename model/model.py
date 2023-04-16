6284555aa0b499217cfbf4318176eac1f15a066c
isuri97 <you@example.com>
2023-04-17 03:35:30 +0530

    print count of truth and pred

--- model/model.py
+++ model/model.py
@@ -162,3 +162,11 @@ labels = ['B-SHIP', 'I-SHIP','B-GHETTO', 'I-GHETTO', 'B-STREET', 'I-STREET', 'B-
 
 print(metrics.classification_report(truths,preds,digits=4))
 
+df_test['original_test_set'] = truths
+df_test['predicted_set'] = preds
+
+result_trt = df_test['original_test_set'] .head(10)
+result_prd = df_test['predicted_set']. head(10)
+
+print(result_trt)
+print(result_prd)
