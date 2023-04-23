6fd09fbebdbcb59aea38fe2bf7dd18f5319cb975
isuri97 <you@example.com>
2023-04-23 19:34:26 +0530

    edit code for newdataset-type2

--- model/model.py
+++ model/model.py
@@ -20,7 +20,7 @@ parser.add_argument('--train', required=False, help='train file', default='data/
 arguments = parser.parse_args()
 
 df_train= pd.read_csv('data/new/cleaned/test-t.csv', sep='\t', error_bad_lines=False,encoding='utf-8')
-df_test = pd.read_csv('data/new/cleaned/gold.csv', sep = '\t')
+df_test = pd.read_csv('data/new/cleaned/gold4.csv', sep = '\t')
 df_test.dropna(subset=['labels'],inplace=True)
 
 
