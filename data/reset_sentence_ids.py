2e3648d68ae1c187f14501c03c330d6694e97e4e
isuri97 <you@example.com>
2023-04-26 08:03:45 +0530

    changes to test set

--- data/reset_sentence_ids.py
+++ data/reset_sentence_ids.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 
 
-df1 = pd.read_csv('new/cleaned/gold4.csv', sep='\t',quoting=csv.QUOTE_NONE, encoding='utf-8')
+df1 = pd.read_csv('new/cleaned/test-t.csv', sep='\t',quoting=csv.QUOTE_NONE, encoding='utf-8')
 df1
 
 sentence_id_list = []
@@ -18,7 +18,7 @@ for word in df1['words'].tolist():
 df1['sentence_ids'] = sentence_id_list
 # df1 = pd.DataFrame({'document_id': df1['sentence_id'], 'words': df1['words'], 'labels': df1['labels']})
 
-df1.to_csv('gold4.csv',index=False,sep='\t')
+df1.to_csv('dataset2.csv',index=False,sep='\t')
 # print('a')
 
 
